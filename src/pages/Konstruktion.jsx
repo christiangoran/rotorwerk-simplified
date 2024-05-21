193b10a183a42e39694e850155962070ca0906b9
christiangoran <christian.goran@protonmail.com>
2024-05-21 18:55:38 +0200

    Add basename = /rotorwerk-simplified to browserRouter in App.jsx

--- src/pages/Konstruktion.jsx
+++ src/pages/Konstruktion.jsx
@@ -89,14 +89,14 @@ export default function Konstruktion() {
         <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
           <div className="lg:pr-4">
             <div className="lg:max-w-lg">
-              <p className="text-base font-semibold leading-7 text-lime-800">
-                Wir konstruieren auch.
+              <p className="text-base font-semibold leading-7 text-yellow-600">
+                We also design.
               </p>
               <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                Konstruktion
+                Constructions
               </h1>
               <p className="mt-6 text-xl leading-8 text-gray-700">
-                Konstruktion für die Branchen und Bereiche:
+                Design for the industries and sectors:
               </p>
             </div>
           </div>
@@ -127,20 +127,21 @@ export default function Konstruktion() {
                   </li>
                 ))}
               </ul>
+
               <p className="mt-8">
-                Soll die Konstruktion auf Verformungen, Verschiebungen,
-                statische Festigkeit und Lebensdauer (Ermüdungsfestigketi)
-                untersucht werden, so setzen wir dafür die{" "}
-                <b>Finite-Elemente-Methode ( FEM Berechnung ) </b>ein.
+                If the construction is to be examined for deformations,
+                displacements, static strength, and lifespan (fatigue strength),
+                we use the <b>Finite Element Method (FEM calculation) </b>
+                for this purpose.
               </p>
               <p className="mt-8">
-                Alles was wir weiterhin für die<b> FEM Berechnung</b> bzw.{" "}
-                <b>Analyse</b> von Ihnen benötigen, sind eventuelle
-                Nachbarkomponenten und deren Eigenschaften, die Lagerung, wenn
-                sich diese in den Nachbarkomponenten befindet und die Belastung.
+                All we further need from you for the<b> FEM calculation</b> or{" "}
+                <b>analysis</b> are any neighboring components and their
+                properties, the bearing if it is located in the neighboring
+                components, and the load.
               </p>
               <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
-                Spezielle Dienstleistungen in diesem Kontext:
+                Special services in this context:
               </h2>
               <ul role="list" className="mt-8 space-y-8 text-gray-600">
                 <li className="flex gap-x-3">
@@ -150,12 +151,8 @@ export default function Konstruktion() {
                   />
                   <span>
                     <strong className="font-semibold text-gray-900">
-                      Neukonstruktion für die oben genannten Branchen und
-                      Bereiche
+                      New design for the aforementioned industries and areas
                     </strong>{" "}
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Maiores impedit perferendis suscipit eaque, iste dolor
-                    cupiditate blanditiis ratione.
                   </span>
                 </li>
                 <li className="flex gap-x-3">
@@ -165,10 +162,10 @@ export default function Konstruktion() {
                   />
                   <span>
                     <strong className="font-semibold text-gray-900">
-                      Änderungskonstruktionen
+                      Modification designs
                     </strong>{" "}
-                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-                    qui lorem cupidatat commodo.
+                    {/* Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
+                    qui lorem cupidatat commodo. */}
                   </span>
                 </li>
                 <li className="flex gap-x-3">
@@ -178,11 +175,11 @@ export default function Konstruktion() {
                   />
                   <span>
                     <strong className="font-semibold text-gray-900">
-                      Festigkeitsberechnungen, Strukturverhalten,
-                      Schwingungsanalysen, ...
+                      Strength calculations, structural behavior, vibration
+                      analyses
                     </strong>{" "}
-                    Ac tincidunt sapien vehicula erat auctor pellentesque
-                    rhoncus. Et magna sit morbi lobortis.
+                    {/* Ac tincidunt sapien vehicula erat auctor pellentesque
+                    rhoncus. Et magna sit morbi lobortis. */}
                   </span>
                 </li>
               </ul>
