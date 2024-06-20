c425dd5d4f0a30f36e8e58ff5b393c30c796e6c7
christiangoran <christian.goran@protonmail.com>
2024-06-20 16:30:57 +0200

    translated konstruktion.jsx to german

--- src/pages/Konstruktion.jsx
+++ src/pages/Konstruktion.jsx
@@ -8,53 +8,53 @@ import konstruktion from "../assets/konstruktion-1.webp";
 
 const industryAreas = [
   {
-    title: "Heavy Machinery Engineering",
+    title: "Schwermaschinenbau",
     description:
-      "We specialize in designing heavy machinery that meets the highest standards of durability and efficiency. Our innovative engineering solutions ensure peak performance in the most demanding environments, making us a trusted partner in the heavy machinery sector.",
+      "Wir sind spezialisiert auf die Konstruktion von Schwermaschinen, die den höchsten Anforderungen an Haltbarkeit und Effizienz entsprechen. Unsere innovativen Ingenieurlösungen sorgen für Spitzenleistungen in den anspruchsvollsten Umgebungen und machen uns zu einem vertrauenswürdigen Partner im Schwermaschinensektor.",
     icon: WrenchIcon,
   },
   {
-    title: "Custom Machinery Engineering",
+    title: "Sondermaschinenbau",
     description:
-      "Tailored solutions for unique challenges. Our custom machinery engineering services deliver specialized equipment designed to meet your specific operational needs. We combine cutting-edge technology with expert craftsmanship to create machines that enhance productivity and efficiency.",
+      "Maßgeschneiderte Lösungen für einzigartige Herausforderungen. Unsere Dienstleistungen im Sondermaschinenbau liefern spezialisierte Ausrüstungen, die auf Ihre spezifischen Betriebsanforderungen zugeschnitten sind. Wir kombinieren modernste Technologie mit fachmännischem Handwerk, um Maschinen zu schaffen, die Produktivität und Effizienz steigern.",
     icon: WrenchIcon,
   },
   {
-    title: "Wind Industry Engineering",
+    title: "Windindustrie-Engineering",
     description:
-      "Driving innovation in the wind industry, we provide comprehensive engineering solutions for wind energy projects. From design to implementation, our expertise ensures optimal performance and reliability of wind turbines and related components, contributing to a sustainable future.",
+      "Innovation in der Windindustrie vorantreiben, bieten wir umfassende Ingenieurlösungen für Windenergieprojekte. Von der Planung bis zur Umsetzung sorgt unser Fachwissen für optimale Leistung und Zuverlässigkeit von Windturbinen und zugehörigen Komponenten und trägt zu einer nachhaltigen Zukunft bei.",
     icon: WrenchIcon,
   },
   {
-    title: "General Machinery Engineering",
+    title: "Allgemeiner Maschinenbau",
     description:
-      "Delivering excellence in mechanical engineering, we cover a broad spectrum of services from concept to completion. Our experienced team applies advanced engineering principles to design, analyze, and optimize mechanical systems for various industrial applications.",
+      "Exzellenz im Maschinenbau liefern wir ein breites Spektrum an Dienstleistungen von der Konzeption bis zur Fertigstellung. Unser erfahrenes Team wendet fortschrittliche Ingenieurprinzipien an, um mechanische Systeme für verschiedene industrielle Anwendungen zu entwerfen, zu analysieren und zu optimieren.",
     icon: WrenchIcon,
   },
   {
-    title: "Structural Steel Engineering",
+    title: "Stahlbau-Engineering",
     description:
-      "Expertise in structural steel design and fabrication. We offer robust and innovative engineering solutions for structural steel projects, ensuring safety, stability, and longevity. Our services cover everything from initial design to final construction.",
+      "Expertise im Stahlbau-Design und in der Fertigung. Wir bieten robuste und innovative Ingenieurlösungen für Stahlbauprojekte und sorgen für Sicherheit, Stabilität und Langlebigkeit. Unsere Dienstleistungen decken alles ab, von der ersten Planung bis zur endgültigen Konstruktion.",
     icon: WrenchIcon,
   },
   {
-    title: "Lightweight Construction Engineering",
+    title: "Leichtbau-Engineering",
     description:
-      "Optimizing performance with lightweight construction. Our engineering solutions focus on reducing weight without compromising strength and durability. Ideal for industries where efficiency and performance are critical, our lightweight designs set the standard.",
+      "Optimierung der Leistung mit Leichtbau. Unsere Ingenieurlösungen konzentrieren sich auf die Gewichtsreduzierung, ohne dabei an Festigkeit und Haltbarkeit einzubüßen. Ideal für Branchen, in denen Effizienz und Leistung entscheidend sind, setzen unsere Leichtbau-Designs Maßstäbe.",
     icon: WrenchIcon,
   },
   {
-    title: "Load Handling Equipment Engineering",
+    title: "Lastenhandhabungsgeräte-Engineering",
     description:
-      "Designing reliable load handling equipment to enhance safety and efficiency. Our engineering services for load handling equipment ensure that your operations run smoothly and safely, with equipment designed to meet rigorous standards and specific requirements.",
+      "Konstruktion zuverlässiger Lastenhandhabungsgeräte zur Verbesserung von Sicherheit und Effizienz. Unsere Ingenieurdienstleistungen für Lastenhandhabungsgeräte sorgen dafür, dass Ihre Abläufe reibungslos und sicher ablaufen, mit Geräten, die den strengen Standards und spezifischen Anforderungen entsprechen.",
     icon: WrenchIcon,
   },
 ];
 
 export default function Konstruktion() {
   return (
-    <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0 text-left">
-      <div className="absolute inset-0 -z-10 overflow-hidden">
+    <div className="relative px-6 py-24 overflow-hidden text-left bg-white isolate sm:py-32 lg:overflow-visible lg:px-0">
+      <div className="absolute inset-0 overflow-hidden -z-10">
         <svg
           className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
           aria-hidden="true"
@@ -85,25 +85,25 @@ export default function Konstruktion() {
           />
         </svg>
       </div>
-      <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+      <div className="grid max-w-2xl grid-cols-1 mx-auto gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
         <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
           <div className="lg:pr-4">
             <div className="lg:max-w-lg">
               <p className="text-base font-semibold leading-7 text-yellow-600">
-                We also create
+                Wir erstellen auch
               </p>
               <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                Constructions
+                Konstruktionen
               </h1>
               <p className="mt-6 text-xl leading-8 text-gray-700">
-                Designs for the industries and sectors:
+                Entwürfe für die Branchen und Sektoren:
               </p>
             </div>
           </div>
         </div>
 
         {/* ------------------ RIGHT ORIENTED IMAGE ELEMENT ------------------ */}
-        <div className="-ml-12 -mt-12 p-12 pb-50 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
+        <div className="p-12 -mt-12 -ml-12 pb-50 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
           <img
             className="w-[48rem] max-w-300 max-h-100 rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
             src={konstruktion}
@@ -117,7 +117,7 @@ export default function Konstruktion() {
               <ul role="list" className="mt-8 space-y-8 text-gray-600">
                 {industryAreas.map((area, index) => (
                   <li key={index} className="flex gap-x-3">
-                    <area.icon className="h-6 w-6 text-lime-900" />
+                    <area.icon className="w-6 h-6 text-lime-900" />
                     <span>
                       <strong className="font-semibold text-gray-900">
                         {area.title}
@@ -129,40 +129,41 @@ export default function Konstruktion() {
               </ul>
 
               <p className="mt-6">
-                If the construction is to be examined for deformations,
-                displacements, static strength, and lifespan (fatigue strength),
-                we use the <b>Finite Element Method (FEM calculation) </b>
-                for this purpose.
+                Wenn die Konstruktion auf Verformungen, Verschiebungen,
+                statische Festigkeit und Lebensdauer (Ermüdungsfestigkeit)
+                untersucht werden soll, verwenden wir dafür die{" "}
+                <b>Finite-Elemente-Methode (FEM-Berechnung)</b>.
               </p>
               <p className="mt-6">
-                All we further need from you for the<b> FEM calculation</b> or{" "}
-                <b>analysis</b> are any neighboring components and their
-                properties, the bearing if it is located in the neighboring
-                components, and the load.
+                Alles, was wir weiter von Ihnen für die <b>FEM-Berechnung</b>{" "}
+                oder <b>Analyse</b> benötigen, sind alle benachbarten
+                Komponenten und deren Eigenschaften, das Lager, falls es sich in
+                den benachbarten Komponenten befindet, und die Last.
               </p>
               <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
-                Special services in this context:
+                Besondere Dienstleistungen in diesem Zusammenhang:
               </h2>
               <ul role="list" className="mt-8 space-y-8 text-gray-600">
                 <li className="flex gap-x-3">
                   <Cog8ToothIcon
-                    className="mt-1 h-5 w-5 flex-none text-lime-700"
+                    className="flex-none w-5 h-5 mt-1 text-lime-700"
                     aria-hidden="true"
                   />
                   <span>
                     <strong className="font-semibold text-gray-900">
-                      New design for the aforementioned industries and areas
+                      Neukonstruktion für die oben genannten Industrien und
+                      Bereiche
                     </strong>{" "}
                   </span>
                 </li>
                 <li className="flex gap-x-3">
                   <CogIcon
-                    className="mt-1 h-5 w-5 flex-none text-lime-700"
+                    className="flex-none w-5 h-5 mt-1 text-lime-700"
                     aria-hidden="true"
                   />
                   <span>
                     <strong className="font-semibold text-gray-900">
-                      Modification designs
+                      Änderungskonstruktionen
                     </strong>{" "}
                     {/* Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
                     qui lorem cupidatat commodo. */}
@@ -170,13 +171,13 @@ export default function Konstruktion() {
                 </li>
                 <li className="flex gap-x-3">
                   <VariableIcon
-                    className="mt-1 h-5 w-5 flex-none text-lime-700"
+                    className="flex-none w-5 h-5 mt-1 text-lime-700"
                     aria-hidden="true"
                   />
                   <span>
                     <strong className="font-semibold text-gray-900">
-                      Strength calculations, structural behavior, vibration
-                      analyses
+                      Festigkeitsberechnungen, Strukturverhalten,
+                      Schwingungsanalysen
                     </strong>{" "}
                     {/* Ac tincidunt sapien vehicula erat auctor pellentesque
                     rhoncus. Et magna sit morbi lobortis. */}
